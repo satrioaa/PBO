a8f1e5cc42807aa3c3505aaa5e9d86753e48bf47
Satrio Arda <75375473+satrioaa@users.noreply.github.com>
2022-03-08 20:26:28 +0700

    first

---
+++ week1/Perkenalan.java
@@ -0,0 +1,8 @@
+public class Perkenalan {
+
+    public static void main(String[] args) {
+        System.out.println("Halo, Nama saya adalah Satrio Arda Alfiansyah");
+        System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+        System.out.println("Saya sedang menulis Java");
+    }
+}
