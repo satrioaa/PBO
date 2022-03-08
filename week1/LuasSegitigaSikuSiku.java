0dbf025ec3379876e7acf34dfd313aeda974dad9
Satrio Arda <75375473+satrioaa@users.noreply.github.com>
2022-03-08 20:24:05 +0700

    Delete LuasSegitigaSikuSiku.java

--- week1/LuasSegitigaSikuSiku.java
+++
@@ -1,9 +0,0 @@
-public class LuasSefitigaSikuSiku {
-    public static void main(String[] args) {
-        double alas = 6;
-        double tinggi = 8;
-        double luas = (alas * tinggi) / 2;
-        System.out.println(
-                "Luas segitiga siku-siku yang mempunyai alas " + alas + " dan tinggi " + tinggi + " adalah " + luas);
-    }
-}
