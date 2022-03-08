4a34f81b9631be0e180d0aecaa9abef84b39589c
Satrio Arda <75375473+satrioaa@users.noreply.github.com>
2022-03-08 20:23:57 +0700

    Delete KelilingLingkaran.java

--- week1/KelilingLingkaran.java
+++
@@ -1,8 +0,0 @@
-public class KelilingLingkaran {
-    public static void main(String[] args) {
-        double diameter = 10;
-        double phi = 3.14;
-        double keliling = phi * diameter;
-        System.out.println("Keliling lingkaran dengan diameter " + diameter + " adalah " + keliling);
-    }
-}
