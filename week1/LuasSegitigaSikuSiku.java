0842ab518ae86493b3afaec5d9d485b5e89d2491
Satrio Arda <75375473+satrioaa@users.noreply.github.com>
2022-03-08 20:19:45 +0700

    Add files via upload

---
+++ week1/LuasSegitigaSikuSiku.java
@@ -0,0 +1,9 @@
+public class LuasSefitigaSikuSiku {
+    public static void main(String[] args) {
+        double alas = 6;
+        double tinggi = 8;
+        double luas = (alas * tinggi) / 2;
+        System.out.println(
+                "Luas segitiga siku-siku yang mempunyai alas " + alas + " dan tinggi " + tinggi + " adalah " + luas);
+    }
+}
